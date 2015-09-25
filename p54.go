b2de0809d3adb404d716e298f85b6f4131cef02a
atlauder <atlauder@gmail.com>
2015-09-25 14:59:04 -0500

    problem 54

--- p54.go
+++ p54.go
@@ -38,6 +38,39 @@ func (h *Hand) getSuits() (suits []string) {
 	return suits
 }
 
+func (h *Hand) String() string {
+	var s string
+
+	for i := 0; i < 5; i++ {
+		if h.cards[i].rank == 10 {
+			s += "10"
+		} else if h.cards[i].rank == 11 {
+			s += "J"
+		} else if h.cards[i].rank == 12 {
+			s += "Q"
+		} else if h.cards[i].rank == 13 {
+			s += "K"
+		} else if h.cards[i].rank == 14 {
+			s += "A"
+		} else {
+			s += strconv.Itoa(h.cards[i].rank)
+		}
+
+		if h.cards[i].suit == "S" {
+			s += "♠ "
+		} else if h.cards[i].suit == "H" {
+			s += "♥ "
+		} else if h.cards[i].suit == "D" {
+			s += "♦ "
+		} else {
+			s += "♣ "
+		}
+
+	}
+
+	return s
+}
+
 type ByRank []Card
 
 func (c ByRank) Len() int {
@@ -114,59 +147,210 @@ func getHands(str string) (Hand, Hand) {
 	return p1Hand, p2Hand
 }
 
-func quads(h Hand) bool {
+func quads(h *Hand) []int {
 	r := h.getRanks()
 	if r[1] == r[2] && r[2] == r[3] {
-		if r[0] == r[1] || r[3] == r[4] {
-			return true
+		if r[0] == r[1] {
+			return append([]int{7}, r...)
+		}
+
+		if r[3] == r[4] {
+			return []int{7, r[1], r[2], r[3], r[4], r[0]}
 		}
 	}
-	return false
+	return nil
 }
 
-func fullHouse(h Hand) bool {
+func fullHouse(h *Hand) []int {
 	r := h.getRanks()
 	if r[0] == r[1] && r[3] == r[4] {
-		if r[2] == r[1] || r[2] == r[3] {
-			return true
+		if r[1] == r[2] {
+			return append([]int{6}, r...)
+		}
+
+		if r[2] == r[3] {
+			return []int{6, r[2], r[3], r[4], r[0], r[1]}
 		}
 	}
-	return false
+	return nil
 }
 
-func flush(h Hand) bool {
-	suits := h.getSuits()
-	for _, v := range suits {
-		if suits[0] != v {
+func flush(h *Hand) []int {
+	s := h.getSuits()
+	r := h.getRanks()
+	for _, v := range s {
+		if s[0] != v {
+			return nil
+		}
+	}
+	return append([]int{5}, r...)
+}
+
+// returns true if the ints in the slice are consecutive
+// and in decending order
+func isConsecutive(nums []int) bool {
+	prev := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if prev-1 != nums[i] {
 			return false
 		}
+		prev = nums[i]
 	}
 	return true
 }
 
-func straight(h Hand) bool {
-	cardRank := h.getRanks()[0]
-	for i := 1; i < 5; i++ {
-		if cardRank-1 != h.cards[i].rank && h.cards[i].rank != 5 {
-			return false
+func straight(h *Hand) []int {
+	r := h.getRanks()
+
+	if isConsecutive(r[1:]) {
+		if r[0] == r[1]+1 {
+			return append([]int{4}, r...)
 		}
-		cardRank = h.cards[i].rank
+
+		if r[0] == 14 && r[1] == 5 {
+			return append([]int{4}, r[1:]...)
+		}
 	}
-	return true
+
+	return nil
 }
 
-func set(h Hand) bool {
-	return false
+// identifies a fullHouse as a set
+func set(h *Hand) []int {
+	r := h.getRanks()
+
+	if r[0] == r[1] && r[1] == r[2] {
+		// if c[3] == c[4] fullHouse
+		return append([]int{3}, r...)
+	}
+	if r[1] == r[2] && r[2] == r[3] {
+		return []int{3, r[1], r[2], r[3], r[0], r[4]}
+	}
+	if r[2] == r[3] && r[3] == r[4] {
+		// if c[0] == c[1] fullHouse
+		return []int{3, r[2], r[3], r[4], r[0], r[1]}
+	}
+	return nil
 }
 
-func twoPair(h Hand) bool {
-	return false
+func twoPair(h *Hand) []int {
+	c := h.getRanks()
+
+	if c[0] == c[1] && c[2] == c[3] {
+		return append([]int{2}, c...)
+	}
+	if c[0] == c[1] && c[3] == c[4] {
+		return []int{2, c[0], c[1], c[3], c[4], c[2]}
+	}
+	if c[1] == c[2] && c[3] == c[4] {
+		return []int{2, c[1], c[2], c[3], c[4], c[0]}
+	}
+	return nil
 }
 
-func pair(h Hand) bool {
-	return false
+func pair(h *Hand) []int {
+	c := h.getRanks()
+	m := make(map[int]int)
+
+	for i := 0; i < 5; i++ {
+		m[c[i]] += 1
+		if m[c[i]] > 1 {
+			// find the kickers... not pait hands
+			kickers := []int{}
+			for k := 0; k < 5; k++ {
+				if k != i && k != i-1 {
+					kickers = append(kickers, c[k])
+				}
+			}
+
+			return append([]int{1, c[i], c[i-1]}, kickers...)
+		}
+	}
+
+	return nil
 }
 
+func highCard(h *Hand) []int {
+	r := h.getRanks()
+	return append([]int{0}, r...)
+}
+
+func setHandValue(h *Hand) []int {
+	f := flush(h)
+	s := straight(h)
+
+	// straight flush
+	if f != nil && s != nil {
+		return append([]int{8}, f...)
+	}
+
+	// quads
+	if c := quads(h); c != nil {
+		return c
+	}
+
+	// fullHouse
+	if c := fullHouse(h); c != nil {
+		return c
+	}
+
+	// flush
+	if f != nil {
+		return f
+	}
+
+	// straight
+	if s != nil {
+		return s
+	}
+
+	// set
+	if c := set(h); c != nil {
+		return c
+	}
+
+	// twoPair
+	if c := twoPair(h); c != nil {
+		return c
+	}
+
+	// pair
+	if c := pair(h); c != nil {
+		return c
+	}
+
+	if c := highCard(h); c != nil {
+		return c
+	}
+
+	// function shouldn't get here
+	return nil
+}
+
+func winner(h1, h2 *Hand) int {
+	one := setHandValue(h1)
+	two := setHandValue(h2)
+
+	fmt.Println(one, two)
+
+	for index, item := range one {
+		if item > two[index] {
+			return 1
+			break
+		} else if item < two[index] {
+			return 2
+			break
+		}
+	}
+
+	// error should not get here
+	// split hand
+	return -1000000000
+
+}
+
+// you can check pair twoPair set fullHouse quads at once
+// by making a map and counting like the pair function
 func main() {
 	lines, err := readLines("./p054_poker.txt")
 	if err != nil {
@@ -177,24 +361,20 @@ func main() {
 	var p1Hand Hand
 	var p2Hand Hand
 
+	p1WinCount := 0
+
 	for i := 1; i <= 1000; i++ {
 
 		p1Hand, p2Hand = getHands(lines[i-1])
-		// fmt.Println("b4", p1Hand)
-		// fmt.Println("b4", p2Hand)
 
-		if fullHouse(p1Hand) {
-			fmt.Println(p1Hand)
+		if winner(&p1Hand, &p2Hand) == 1 {
+			p1WinCount += 1
 		}
-
-		if fullHouse(p2Hand) {
-			fmt.Println(p2Hand)
-		}
-
-		// do hand eval here
 
 		p1Hand = Hand{}
 		p2Hand = Hand{}
 	}
+
+	fmt.Println(p1WinCount) // 376
 
 }
