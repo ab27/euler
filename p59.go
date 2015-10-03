1bc46b8154fa206eccfdd5c8d0db61caaeb13987
atlauder <atlauder@gmail.com>
2015-10-02 19:54:09 -0500

    problem 59

---
+++ p59.go
@@ -0,0 +1,127 @@
+// Each character on a computer is assigned a unique code and the
+// preferred standard is ASCII (American Standard Code for Information
+// Interchange). For example, uppercase A = 65, asterisk (*) = 42, and
+// lowercase k = 107.
+
+// A modern encryption method is to take a text file, convert the bytes
+// to ASCII, then XOR each byte with a given value, taken from a secret
+// key. The advantage with the XOR function is that using the same
+// encryption key on the cipher text, restores the plain text; for example,
+// 65 XOR 42 = 107, then 107 XOR 42 = 65.
+
+// For unbreakable encryption, the key is the same length as the plain
+// text message, and the key is made up of random bytes. The user would
+// keep the encrypted message and the encryption key in different
+// locations, and without both "halves", it is impossible to decrypt the
+// message.
+
+// Unfortunately, this method is impractical for most users, so the
+// modified method is to use a password as a key. If the password is
+// shorter than the message, which is likely, the key is repeated
+// cyclically throughout the message. The balance for this method is
+// using a sufficiently long password key for security, but short enough
+// to be memorable.
+
+// Your task has been made easy, as the encryption key consists of three
+// lower case characters. Using cipher.txt (right click and 'Save
+// Link/Target As...'), a file containing the encrypted ASCII codes, and
+// the knowledge that the plain text must contain common English words,
+// decrypt the message and find the sum of the ASCII values in the original
+// text.
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var p = fmt.Println
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println("Fatal error ", err.Error())
+		os.Exit(1)
+	}
+}
+
+// readLines reads a whole file into memory
+// and returns a slice of its lines.
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+// find the sum of the ASCII values in the original text
+func calcSum(ascii []int) (total int) {
+	total = 0
+	for _, v := range ascii {
+		total += v
+	}
+	return
+}
+
+// generate all possible keys
+func generateKeys() [][]int {
+	// 97 to 122
+	keys := [][]int{}
+
+	for msd := 97; msd <= 122; msd++ {
+		for middle := 97; middle <= 122; middle++ {
+			for lsd := 97; lsd <= 122; lsd++ {
+				keys = append(keys, []int{msd, middle, lsd})
+			}
+		}
+	}
+	return keys
+}
+
+func main() {
+	lines, err := readLines("./p059_cipher.txt")
+	checkError(err)
+
+	words, err := readLines("./wordsEn.txt")
+	checkError(err)
+
+	wordMap := make(map[string]bool)
+	// inserts words to map
+	for _, v := range words {
+		wordMap[v] = true
+	}
+
+	charSlice := strings.Split(lines[0], ",")
+	cipherTxt := []int{}
+
+	keys := generateKeys()
+
+	// convert charSlice elements from string to int
+	for _, v := range charSlice {
+		num, err := strconv.Atoi(v)
+		checkError(err)
+		cipherTxt = append(cipherTxt, num)
+	}
+
+	// for _, v := range []int{65, 67, 32, 100, 97, 70} {
+	for _, v := range cipherTxt {
+		fmt.Print(string(byte(v)))
+	}
+	p()
+	p(string(byte(65 ^ 42)))
+	p(string(byte(107 ^ 42)))
+
+	p(len(cipherTxt), 26*26*26, len(words), wordMap["ass"], len(keys), keys[0])
+
+}
--- p88.go
+++ p88.go
@@ -66,7 +66,113 @@ func products(num, size int) [][]int {
 	return result
 }
 
-func main() {
-	fmt.Println(isProductSum([]int{1, 1, 1, 1, 2, 6}))
-	fmt.Println(products(7, 4))
+// 5, 4, 4, 2, 2, 2, 1
+func findGiverReceiver(p []int) ([]int, bool) {
+	// start from back and look for intersection
+	s := p[len(p)-1] // smallest
+	si := len(p) - 1 // smallest index
+
+	for i := len(p) - 2; i >= 1; i-- {
+		if p[i] != s {
+			si = i + 1
+			break
+		}
+	}
+
+	for i := (si - 1); i >= 1; i-- {
+		if (p[i] - p[si]) >= 2 {
+			return []int{i, si}, true
+		}
+	}
+
+	return []int{}, false
 }
+
+func nextPart(p []int, sum int) []int {
+	// can something be given in p[1:]
+	//   if so who should give to whom
+	// if nothing can be given take 1 from p[0]
+
+	next := make([]int, len(p))
+	copy(next, p)
+
+	if pair, ok := findGiverReceiver(p); ok {
+		next[pair[0]] -= 1
+		next[pair[1]] += 1
+		return next
+	}
+
+	leftOver := sum - (p[0] - 1) - (len(p) - 2)
+	fmt.Println("leftOver", leftOver)
+
+	if p[0]-1 >= leftOver {
+		next[0] = p[0] - 1
+		next[1] = leftOver
+		for i := 2; i < len(p); i++ {
+			next[i] = 1
+		}
+		return next
+	} else {
+		next[0] = p[0] - 1
+		next[1] = p[0] - 1
+
+		for i := 2; i < len(p); i++ {
+			next[i] = 1
+		}
+
+		left := sum - (2 * next[0]) - (len(p) - 2)
+		// fmt.Println("left", left)
+
+		// give away the left over
+		for i := 2; i < len(p); i++ {
+			if (left + next[i]) <= next[0] {
+				next[i] += left
+				break
+			} else {
+				next[i] = next[0]
+				left = left - (next[0] - 1)
+			}
+		}
+
+		return next
+	}
+
+	// return done
+	return []int{}
+}
+
+func partitions(num, size int) [][]int {
+	result := [][]int{}
+
+	part := []int{num - size + 1}
+	for i := 0; i < size-1; i++ {
+		part = append(part, 1)
+	}
+
+	result = append(result, part)
+
+	return result
+}
+
+func main() {
+	// fmt.Println(isProductSum([]int{1, 1, 1, 1, 2, 6}))
+	// fmt.Println(products(7, 4))
+	// fmt.Println(partitions(20, 7))
+
+	// ret, ok := findGiverReceiver([]int{19, 3, 3, 2, 1, 1, 1})
+	// fmt.Println(ret, ok)
+
+	// ret, ok := findGiverReceiver([]int{5, 4, 4, 2, 2, 2, 1})
+	// fmt.Println(ret, ok)
+
+	fmt.Println(nextPart([]int{5, 3, 3, 3, 2, 2, 2}, 20))
+}
+
+/*
+[5 5 5 2 1 1 1]
+[5 5 4 2 2 1 1]
+[5 5 3 2 2 2 1]
+[5 5 2 2 2 2 2]
+[5 4 3 2 2 2 2]
+[5 3 3 3 2 2 2]
+*/
