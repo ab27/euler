438b2e51d14c06612acf90ce4bda501dd9b578ef
Abe Letta <atlauder@gmail.com>
2015-10-23 16:31:23 -0500

    Largest palindrome product

--- p4.go
+++ p4.go
@@ -2,7 +2,7 @@
 // made from the product of two 2-digit numbers is 9009 = 91 × 99.
 
 // Find the largest palindrome made from the product of two 3-digit
-// numbers.
+// numbers. 
 
 package main
 
