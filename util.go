6e5f5877ad14fd675f497a619fbfe93802d59cdc
Abe <atlauder@gmail.com>
2015-10-31 13:59:16 -0500

    Update util.go

--- util.go
+++ util.go
@@ -3,6 +3,16 @@ import (
 	"os"
 )
 
+public static class Tuple<X, Y> { 
+  public final X x; 
+  public final Y y; 
+  public Tuple(X x, Y y) { 
+    this.x = x; 
+    this.y = y; 
+  } 
+}
+
+
 import java.util.Scanner;
 
 public class RecursiveNestedLoops {
